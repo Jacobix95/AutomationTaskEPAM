f269c6bb9dc11b6d9d9371642768b5273dc482cf
Jakub <kuba.reszel@gmail.com>
2025-06-30 11:21:33 +0200

    restrat poject for add all to PR

--- src/test/java/strategy/BrowserStrategy.java
+++
@@ -1,7 +0,0 @@
-package strategy;
-
-import org.openqa.selenium.WebDriver;
-
-public interface BrowserStrategy {
-    WebDriver createDriver();
-}
--- src/test/java/utils/ErrorMessage.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class ErrorMessage {
-    public static final String USERNAME_REQUIRED = "Epic sadface: Username is required";
-    public static final String PASSWORD_REQUIRED = "Epic sadface: Password is required";
-}
