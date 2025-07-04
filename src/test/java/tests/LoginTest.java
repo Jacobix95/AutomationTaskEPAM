6488b6a517869e3e2546b06a95afd93b8ddf87f6
Jacobix95 <145839749+Jacobix95@users.noreply.github.com>
2025-07-04 06:59:45 +0000

    Add BDD-style comments with given/when/then in LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -13,37 +13,49 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void testEmptyCredentials_UC1() {
+        // given
         getDriver().get(ConfigReader.getBaseUrl());
-
         LoginPage login = new LoginPage(getDriver());
+
+        // when
         login.enterUsername("testUser");
         login.enterPassword("testPass");
         login.clearUsername();
         login.clearPassword();
         login.clickLogin();
+        
+        //then
         Assertions.assertThat(login.getErrorMessage()).isEqualTo(USERNAME_REQUIRED);
     }
 
     @Test
     public void testUsernameOnly_UC2() {
+        // given
         getDriver().get(ConfigReader.getBaseUrl());
-
         LoginPage login = new LoginPage(getDriver());
+
+        // when
         login.enterUsername("standard_user");
         login.enterPassword("anyPass");
         login.clearPassword();
         login.clickLogin();
+
+        // then
         Assertions.assertThat(login.getErrorMessage()).isEqualTo(PASSWORD_REQUIRED);
     }
 
     @Test
     public void testValidCredentials_UC3() {
+        //given
         getDriver().get(ConfigReader.getBaseUrl());
-
         LoginPage login = new LoginPage(getDriver());
+        
+        // when
         login.enterUsername("standard_user");
         login.enterPassword("secret_sauce");
         login.clickLogin();
+        
+        //then
         Assertions.assertThat(getDriver().getCurrentUrl()).contains("inventory");
     }
 }
